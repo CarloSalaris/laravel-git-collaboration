541cb8861db4ae9ec919802a7cb233c6eebf282a
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-07-21 15:09:26 +0100

    add seeder

--- database/seeders/VanTableSeeder.php
+++ database/seeders/VanTableSeeder.php
@@ -22,9 +22,6 @@ class VanTableSeeder extends Seeder
             $van = new Van();
             $van->brand = $faker->company();
 
-            $van->price = $faker->numberBetween(999, 29999);
-            $van->seat = $faker->randomElements([5, 7, 9]);
-
             $van->save();
         }
     }
